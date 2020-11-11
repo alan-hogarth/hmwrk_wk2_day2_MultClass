bbcfdbfb362e1b599837dca4982f56d6739b2a0b
Alan Hogarth <alan.hogarth@outlook.com>
2020-11-11 08:50:17 +0000

    final commit

--- src/bus_stop.py
+++ src/bus_stop.py
@@ -9,3 +9,8 @@ class BusStop:
     def add_to_queue(self, person):
         self.queue.append(person)
         
+# Try writing a method that the bus would call, to collect all of the passengers from a stop. This might look like bus.pick_up_from_stop(self, bus_stop_1). This should take all of the passengers waiting in line at the stop, and put them inside of the bus. So the stop will now have nobody in the queue, and the number of people on the bus will increase by however many people the stop had at it.
+
+    def pick_up_from_stop(self, bus_stop_1):
+        
+    
